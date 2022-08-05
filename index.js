4a5f1d3a2d42ef75fa2a3ca69e48ba520ea81616
Ivan <nguyoivan@gmail.com>
2022-08-05 15:12:39 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-// Code your solutions in this file
 function writeCards( names) {
-    let thankCards = []
-    for ( let i = 0; i < names.length; i++ ) {
-      thankCards.push( `Thank you, ${names[i]}, for the wonderful surprise gift!` )
-    }
-    return thankCards
+  let thankCards = []
+  for ( let i = 0; i < names.length; i++ ) {
+    thankCards.push( `Thank you, ${names[i]}, for the wonderful surprise gift!` )
   }
-  
-  function countDown( start ) {
-    while ( start > 0 ) {
-      console.log( start );
-      start -= 1;
-    }
-    console.log( start );
+  return thankCards
+}
+
+function countDown( start ) {
+  let countup = 0;
+while (countup < 11) {
+console.log(countup++);
+}
   }
+  console.log( start );
